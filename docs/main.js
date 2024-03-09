ee696cf4ffab08a48ef4cf26af04e11d532642aa
Václav Chvíla <v.chvila@seznam.cz>
2024-03-09 09:56:36 +0100

    [add] basic html struct and js file

---
+++ docs/main.js
@@ -0,0 +1,66 @@
+"use strict";
+var LamaGoCore = (function () {
+    function LamaGoCore() {
+        var url = new URL(window.location.href);
+        this.currentParams = url.searchParams;
+        this.size = this.parseSize();
+    }
+    LamaGoCore.prototype.parseSize = function () {
+        var size = this.currentParams.get("size");
+        if (size === null) {
+            return LamaGoCore.DEFAULT_SIZE;
+        }
+        else {
+            var parsedNumber = parseInt(size, 10);
+            if (isNaN(parsedNumber)) {
+                return LamaGoCore.DEFAULT_SIZE;
+            }
+            else {
+                return parsedNumber;
+            }
+        }
+    };
+    LamaGoCore.prototype.getSize = function () {
+        return this.size;
+    };
+    LamaGoCore.DEFAULT_SIZE = 49;
+    return LamaGoCore;
+}());
+var LamaGoBoard = (function () {
+    function LamaGoBoard(size) {
+        this.board = document.getElementById("board");
+        this.boardEdges = document.getElementById("board-edges");
+        this.delimiterXTemplate = document.getElementById("delimiter-x-template");
+        this.delimiterYTemplate = document.getElementById("delimiter-y-template");
+        this.stoneTemplate = document.getElementById("stone-template");
+        this.stoneSize = 100 / (size - 1);
+        this.edgeSize = this.stoneSize / 2;
+        if (this.boardEdges !== null
+            && this.delimiterXTemplate !== null) {
+            this.boardEdges.style.top = this.edgeSize + "%";
+            this.boardEdges.style.left = this.edgeSize + "%";
+            this.boardEdges.style.width = (100 - this.stoneSize) + "%";
+            this.boardEdges.style.height = this.boardEdges.style.width;
+            for (var i = 0; i < size; i++) {
+                var cloneX = this.delimiterXTemplate.content.cloneNode(true);
+                if (cloneX.firstElementChild !== null
+                    && cloneX.firstElementChild instanceof HTMLElement) {
+                    cloneX.firstElementChild.style.left = (this.stoneSize * i) + "%";
+                    this.boardEdges.appendChild(cloneX);
+                }
+                var cloneY = this.delimiterYTemplate.content.cloneNode(true);
+                if (cloneY.firstElementChild !== null
+                    && cloneY.firstElementChild instanceof HTMLElement) {
+                    cloneY.firstElementChild.style.top = (this.stoneSize * i) + "%";
+                    this.boardEdges.appendChild(cloneY);
+                }
+            }
+        }
+    }
+    return LamaGoBoard;
+}());
+document.addEventListener("DOMContentLoaded", function (event) {
+    var core = new LamaGoCore();
+    var board = new LamaGoBoard(core.getSize());
+});
+//# sourceMappingURL=main.js.map
---
+++ src/main.ts
@@ -0,0 +1,84 @@
+class LamaGoCore {
+    private static DEFAULT_SIZE: number = 19;
+    private currentParams: URLSearchParams;
+    private size: number;
+
+    public constructor() {
+        const url = new URL(window.location.href);
+        this.currentParams = url.searchParams;
+
+        this.size = this.parseSize();
+    }
+
+    private parseSize(): number {
+        let size = this.currentParams.get("size");
+        if (size === null) {
+            return LamaGoCore.DEFAULT_SIZE;
+        } else {
+            let parsedNumber = parseInt(size, 10);
+            if (isNaN(parsedNumber)) {
+                return LamaGoCore.DEFAULT_SIZE;
+            } else {
+                return parsedNumber;
+            }
+        }
+    }
+
+    public getSize(): number {
+        return this.size;
+    }
+}
+
+class LamaGoBoard {
+    private board: HTMLElement | null;
+    private boardEdges: HTMLElement | null;
+    private delimiterXTemplate: HTMLTemplateElement | null;
+    private delimiterYTemplate: HTMLTemplateElement | null;
+    private stoneTemplate: HTMLElement | null;
+
+    private stoneSize: number;
+    private edgeSize: number;
+
+    public constructor(size: number) {
+        this.board = document.getElementById("board");
+        this.boardEdges = document.getElementById("board-edges");
+        this.delimiterXTemplate = document.getElementById("delimiter-x-template") as HTMLTemplateElement;
+        this.delimiterYTemplate = document.getElementById("delimiter-y-template") as HTMLTemplateElement;
+        this.stoneTemplate = document.getElementById("stone-template");
+
+        this.stoneSize = 100 / (size - 1);
+        this.edgeSize = this.stoneSize / 2;
+
+        // Configure
+        if (this.boardEdges !== null
+            && this.delimiterXTemplate !== null) {
+            // Edges
+            this.boardEdges.style.top = this.edgeSize + "%";
+            this.boardEdges.style.left = this.edgeSize + "%";
+            this.boardEdges.style.width = (100 - this.stoneSize) + "%";
+            this.boardEdges.style.height = this.boardEdges.style.width;
+
+            // Generate rows and columns
+            for (let i = 0; i < size; i++) {
+                let cloneX = this.delimiterXTemplate.content.cloneNode(true) as HTMLElement;
+                if (cloneX.firstElementChild !== null
+                    && cloneX.firstElementChild instanceof HTMLElement) {
+                    cloneX.firstElementChild.style.left = (this.stoneSize * i) + "%";
+                    this.boardEdges.appendChild(cloneX);
+                }
+
+                let cloneY = this.delimiterYTemplate.content.cloneNode(true) as HTMLElement;
+                if (cloneY.firstElementChild !== null
+                    && cloneY.firstElementChild instanceof HTMLElement) {
+                    cloneY.firstElementChild.style.top = (this.stoneSize * i) + "%";
+                    this.boardEdges.appendChild(cloneY);
+                }
+            }
+        }
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    var core = new LamaGoCore();
+    var board = new LamaGoBoard(core.getSize());
+});
